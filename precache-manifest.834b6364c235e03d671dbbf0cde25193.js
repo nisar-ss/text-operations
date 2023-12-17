3e4942a8dd3ec33c375fd9957f04d6406424c02e
nisar78 <shaikhnisar107@gmail.com>
2023-12-17 13:28:43 +0530

    Updates

---
+++ precache-manifest.834b6364c235e03d671dbbf0cde25193.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54d264f679fa452e8abf92287da93861",
+    "url": "/text-ops/index.html"
+  },
+  {
+    "revision": "bc999bb37be2e73ef716",
+    "url": "/text-ops/static/css/main.171a055b.chunk.css"
+  },
+  {
+    "revision": "0241ed0ac590b6d07585",
+    "url": "/text-ops/static/js/2.f36ee066.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/text-ops/static/js/2.f36ee066.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e2716537566e4fd0df6",
+    "url": "/text-ops/static/js/3.2fa43014.chunk.js"
+  },
+  {
+    "revision": "bc999bb37be2e73ef716",
+    "url": "/text-ops/static/js/main.9aa4cfa8.chunk.js"
+  },
+  {
+    "revision": "723f85806924d3922833",
+    "url": "/text-ops/static/js/runtime-main.1ed95e32.js"
+  }
+]);
